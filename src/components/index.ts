4bb05514c6ed77ab8d84109733a698debc27e764
le-ar <andreiklauzer@gmail.com>
2024-01-22 18:02:51 +0300

    fix: exports DragDropView

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './DragDropContextRootView';
 export * from './DragDropItem';
 export * from './DragDropScrollView';
+export * from './DragDropView';
 export * from './HoveredItem';
 export * from './HoveredItemContextRootProvider';
